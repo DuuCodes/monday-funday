0320410cf1ba4257cf1802e7c94c56c26e487e51
Dawdu <dawduwrites@gmail.com>
2019-09-30 21:14:30 -0400

    show examples of data types

---
+++ data-types.js
@@ -0,0 +1,15 @@
+var a = 1;
+var string = ('helloWorld')
+var name = 'openSesame'
+var Array = [1,2,3,4,5,6,7,8,9]
+var anotherDate = ('2019-11-11')
+
+
+
+console.log(Boolean(5))  
+console.log(String(4))
+console.log(Number(3,4,5,6))
+ 
+var Numlist = [1,2,3,4,5]
+ numList.map(plusOne)
+
