1face5a669f434692316c60b4059c703e7fa702f
Dawdu <dawduwrites@gmail.com>
2019-11-15 13:22:29 -0500

    environment examples for arrays,host and filter functions

--- environment_variable.js
+++ environment_variable.js
@@ -1,5 +1,6 @@
-//require = ('dotenv').config()
+require('dotenv').config()
 // this can be tested using MY_MEM=Arrays node environment_varaible.js
+//MY_MEM is an env variable making up just for sake of testing env vars out
 const myArrays = process.env.MY_MEM || 'Arrays';
 console.log('I remember ' + myArrays)
 
@@ -7,8 +8,8 @@ console.log('I remember ' + myArrays)
 const filters = process.env.MY_FA || 'filter functions';
 console.log('I really like the ' + filters)
 
-// this can be called so that the defualt is used and also with a specific DB_Host
+// this can be called so that the default is used and also with a specific DB_Host
 // MY_MEM=Arrays node environment_variables.js
-//
+
 const db_host = process.env.DB_Host || 'localhost';
 console.log('Using database host: ' + db_host)
